e69d145f8468c1f039c02c7f4fe386b296aab6c9
Wyatt Tauber <wwt9829@g.rit.edu>
2019-11-23 19:34:34 -0800

    activity 5 travis.yml update

--- tests/test_act5.py
+++ tests/test_act5.py
@@ -6,11 +6,12 @@ def test_classic_sql():
     """
     Classic Sql injection Test
     """
-    classic = requests.post("http://localhost:5000/login", {"username": "admin@user.com\' OR \'1\'=\'1", "password": ""}).text
+    classic = requests.post("http://localhost:5001/login", {"username": "admin@user.com\' OR \'1\'=\'1", "password": ""}).text
     soup = BeautifulSoup(classic, "html.parser")
-    text=soup.find_all(text=True)
-    print(text)
-    assert "7476de220a716fec6159e5f9129b4caf80e052c850531cf3291b9abefd831400" in text
+    
+    body = soup.find('p').text
+
+    assert "7476de220a716fec6159e5f9129b4caf80e052c850531cf3291b9abefd831400" in body
 
 if __name__ == '__main__':
     test_classic_sql()
