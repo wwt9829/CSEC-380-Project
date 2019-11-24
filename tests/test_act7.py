97adf2061cdde88b79ab0b34ddff1057126d7f3b
Connor Shade <cws4197@rit.edu>
2019-11-24 05:12:42 +0000

    Fix act7 testing

--- tests/test_act7.py
+++ tests/test_act7.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+from os import urandom
 
 
 # Activity 7 test
 def test_command_injection():
-    login = requests.get("http://localhost:5000/adduser", {"username": "uniquename", "password": "password", "displayname": "uniquename", "command": "whoami"}).text
+    # Create unique name
+    name = urandom(8).hex()
+    login = requests.get("http://localhost:5000/adduser", {"username": name, "password": "password", "displayname": name, "command": "whoami"}).text
     soup = BeautifulSoup(login, "html.parser")
     text=soup.find_all(text=True)
     print(text)
-    assert "root" in text
+    assert "root\n" in text
 
 if __name__ == '__main__':
     test_command_injection()
