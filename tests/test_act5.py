cf110a9be85b46f3adf1c85a1709a0aa1edd0dd8
Wyatt Tauber <wwt9829@g.rit.edu>
2019-11-23 20:22:59 -0800

    add more sleep to act 5

--- tests/test_act5.py
+++ tests/test_act5.py
@@ -9,12 +9,19 @@ def test_classic_sql():
     """
     Classic Sql injection Test
     """
-    classic = requests.post("http://localhost:5001/login", {"username": "admin@user.com\' OR \'1\'=\'1", "password": ""}).text
-    soup = BeautifulSoup(classic, "html.parser")
-    
-    body = soup.find('p').text
+    i = 0
+    while i < 15:
+        try:
+            classic = requests.post("http://localhost:5001/login", {"username": "admin@user.com\' OR \'1\'=\'1", "password": ""}).text
+            soup = BeautifulSoup(classic, "html.parser")
+            
+            body = soup.find('p').text
 
-    assert "7476de220a716fec6159e5f9129b4caf80e052c850531cf3291b9abefd83140" in body
+            assert "7476de220a716fec6159e5f9129b4caf80e052c850531cf3291b9abefd83140" in body
+        except Exception:
+            print("Connection timed out... retry #", i)
+            time.sleep(3)
+            i += 1
 
 if __name__ == '__main__':
     test_classic_sql()
