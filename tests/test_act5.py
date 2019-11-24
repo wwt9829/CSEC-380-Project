0d4efa5b38f83ad638d50a84de1c41d04adfaf8c
Shannon McHale <46467306+Littlehack3r@users.noreply.github.com>
2019-11-23 22:00:02 -0500

    Update test_act5.py

--- tests/test_act5.py
+++ tests/test_act5.py
@@ -2,15 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def classic_sql():
+def test_classic_sql():
     """
     Classic Sql injection Test
     """
     classic = requests.post("http://localhost:5000/login", {"username": "admin@user.com\' OR \'1\'=\'1", "password": ""}).text
     soup = BeautifulSoup(classic, "html.parser")
     text=soup.find_all(text=True)
+    string = ""
+    for x in text:
+        string += str(x)
+    text = string
     print(text)
-    assert "7476de220a716fec6159e5f9129b4caf80e052c850531cf3291b9abefd831400" in text
+    assert "Error" in text
 
 if __name__ == '__main__':
-    sclassic_sql()
+    test_classic_sql()
