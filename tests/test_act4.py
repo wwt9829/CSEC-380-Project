cc50a14ec86560346be911cc41996abb5b2ec3e3
mikelanzafame <mal8133@rit.edu>
2019-11-23 21:01:13 -0500

    Fix for test_act4
    Test for act 4 now passes pytest, and the changes made by the code are reflected on the webapp. Assertion statements have room for improvement. Note the comment in lines 16-17 in regards to delete_video.

--- tests/test_act4.py
+++ tests/test_act4.py
@@ -1,31 +1,22 @@
 import requests
+import pytest
 
 # Activity 4 test. Code signs in as a user, uploads a video, views the video, then deletes the video.
 def test_act4():
-    verify_upload = False
-    verify_view = False
-    verify_delete = True
-
     session = requests.session()
     login = session.post("http://localhost:5000/login", {"username": "test@user.com", "password": "password"}).text
 
-    with open('./tests/small.mp4', 'rb') as file:
-        upload_file = session.post("http://localhost:5000/home", data=file).text
-    if "small.mp4" in upload_file:
-        verify_upload = True
+    with open('small.mp4', 'rb') as file:
+        upload_file = session.post("http://localhost:5000/home", data={'submit': 'Upload'}, files={'file': file}).text
+    assert "Test Test's ChaimTube" in upload_file   # Not sure what to assert here
 
     view_video = session.get("http://localhost:5000/video/small.mp4").status_code
-    if view_video == 200:
-        verify_view = True
+    assert view_video == 200                        # Only thing I found usable was the status code
 
-    # delete_video = session.get("http://localhost:5000/delete/1").text
-    # if "small.mp4" not in delete_video:
-    #     verify_delete = True
-
-    session.close()
-
-    return "Upload successful? --> " and verify_upload and "\n" and "View successful? --> " and verify_view \
-        and "\n" and "Delete successful? --> " and verify_delete and "\n"
+    # Video ID is only 1 the first time this test is ran. It will not work if videos have been added before running.
+    # In order to work after that first initial run, the 1 must be changed to match the ID of the video to delete.
+    delete_video = session.get("http://localhost:5000/delete/1").text
+    assert "Test Test's ChaimTube" in delete_video  # Again, not sure what else to assert
 
 if __name__ == '__main__':
     test_act4()
